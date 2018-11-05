64cc57451157479d8ad67401911140978676ca30
Li Dong <lidong8@le.com>
2018-11-05 18:25:50 +0800

    修复math符号的问题和评论

--- patch/run.sh
+++ patch/run.sh
@@ -4,6 +4,8 @@
 # npm install hexo-renderer-kramed
 # npm install kramed
 
-cp ./renderer.js ../node_modules/hexo-renderer-kramed/lib/renderer.js
+# cp ./renderer.js ../node_modules/hexo-renderer-kramed/lib/renderer.js
+# cp ./inline.js ../node_modules/kramed/lib/rules/inline.js
 cp ./mathjax.html ../node_modules/hexo-renderer-mathjax/mathjax.html
-cp ./inline.js ../node_modules/kramed/lib/rules/inline.js
+# cp ./gitment.js ../node_modules/gitment/dist/gitment.js
+# 或者修改：themes/hexo-theme-indigo/layout/_partial/plugins/gitment.ejs

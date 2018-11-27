b6280bc814327384f8a45c8b4736f2bf12c428e1
Li Dong <lidong8@le.com>
2018-11-27 22:01:52 +0800

    integrate jupyter-notebook on my blog

---
+++ patch/hexo-jupyter-notebook/main.py
@@ -0,0 +1,45 @@
+"""
+jupyter convert
+"""
+
+from __future__ import print_function
+import sys
+import re
+from nbconvert import HTMLExporter
+
+
+def main(asset_dir, jupyter_file):
+    """
+    convert jupyter file to html
+    :params jupyter_file: juptyer file path
+    """
+    html_exporter = HTMLExporter()
+    html_exporter.template_file = 'full'
+
+    # lidong add
+    import os
+    dn = os.path.dirname(asset_dir)
+    if os.path.exists(dn + ".md"):
+        dn = os.path.dirname(dn)
+        jupyter_file = os.path.join(dn, jupyter_file)
+    else:
+        print("not found: " + os.path.join(asset_dir, jupyter_file))
+        return
+
+    restr = "%s" % (str(html_exporter.from_filename(jupyter_file)[0]))
+    # lidong mod, jquery only use 2.0.0, other have some problems
+    template = """
+<script src="http://code.jquery.com/jquery-2.0.0.js"></script>
+<iframe id="ipynb" marginheight="0" marginwidth="0" frameborder="0" width="100%%" srcdoc="%s" style="scrolling:no;">
+</iframe>
+<script>
+$("#ipynb").load( function() {
+document.getElementById('ipynb').height=$("#ipynb").contents().find("#notebook").height()+100;
+})
+</script> 
+    """ % restr.replace("\"", "'")
+    # print(sys.version)
+    # template = '2341'
+    print(re.sub(r'<a.*?\/a>', '', template))
+
+main(sys.argv[1], sys.argv[2])
--- patch/run.sh
+++ patch/run.sh
@@ -4,8 +4,10 @@
 # npm install hexo-renderer-kramed
 # npm install kramed
 
-# cp ./renderer.js ../node_modules/hexo-renderer-kramed/lib/renderer.js
-# cp ./inline.js ../node_modules/kramed/lib/rules/inline.js
-cp ./mathjax.html ../node_modules/hexo-renderer-mathjax/mathjax.html
-# cp ./gitment.js ../node_modules/gitment/dist/gitment.js
+# cp ./hexo-renderer-kramed/renderer.js ../node_modules/hexo-renderer-kramed/lib/
+# cp ./kramed/inline.js ../node_modules/kramed/lib/rules/
+cp ./hexo-renderer-mathjax/mathjax.html ../node_modules/hexo-renderer-mathjax/
+cp ./hexo-jupyter-notebook/main.py ../node_modules/hexo-jupyter-notebook/
+cp ./hexo-jupyter-notebook/index.js ../node_modules/hexo-jupyter-notebook/
+# cp ./gitment/gitment.js ../node_modules/gitment/dist/
 # 或者修改：themes/hexo-theme-indigo/layout/_partial/plugins/gitment.ejs

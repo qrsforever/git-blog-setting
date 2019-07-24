901356b911d63d9b0fd50c5423ac89518803eb37
Li Dong <lidong8@le.com>
2019-07-24 22:35:48 +0800

    add md5 for svg

--- graphviz.py
+++ graphviz.py
@@ -8,6 +8,7 @@ Requires pygraphviz, pandocfilters
 
 import os
 import sys
+import hashlib
 
 import pygraphviz
 
@@ -26,36 +27,49 @@ def graphviz(key, value, format, meta):
             prog, keyvals = get_value(keyvals, u"prog", u"dot")
             filetype = get_extension(format, "svg", html="svg", latex="pdf")
             # dest = get_filename4code(graph, code, filetype)
+            md5 = hashlib.sha1(code.encode(sys.getfilesystemencoding())).hexdigest()
             filename, _ = get_value(keyvals, u"filename")
             if filename is None:
                 sys.stderr.write('not set filename in {}\n')
                 exit(-1)
-            try:
-                datapath = meta['datapath']['c']
-                drafts_idx = datapath.find('_drafts')
-                if drafts_idx > 0:
-                    prefix = datapath[drafts_idx+8:-3]
-                else:
-                    prefix = datapath[datapath.find('_posts')+7:-3]
-                filepath = os.path.join(prefix, dir_name, filename)
-                if drafts_idx > 0:
-                    localpath = os.path.join(top_path, "source/assets", filepath)
-                    remotepath = os.path.join("/assets", filepath)
-                else:
-                    localpath = os.path.join(top_path, "source/_assets", filepath)
-                    remotepath = os.path.join(git_path, filepath)
-                dir = os.path.dirname(localpath)
-                if not os.path.isdir(dir):
-                    os.makedirs(dir)
-            except Exception as e:
-                sys.stderr.write('{}: not found datapath in meta, please patch/run.sh\n'.format(e))
-                exit(-1)
+            filename += '.' + filetype
+            while True:
+                try:
+                    datapath = meta['datapath']['c']
+                    drafts_idx = datapath.find('_drafts')
+                    if drafts_idx > 0:
+                        prefix = datapath[drafts_idx+8:-3]
+                    else:
+                        prefix = datapath[datapath.find('_posts')+7:-3]
+                    filepath = os.path.join(prefix, dir_name, filename)
+                    if drafts_idx > 0:
+                        localpath = os.path.join(top_path, "source/assets", filepath)
+                        remotepath = os.path.join("/assets", filepath)
+                    else:
+                        localpath = os.path.join(top_path, "source/_assets", filepath)
+                        remotepath = os.path.join(git_path, filepath)
+                    if os.path.exists(localpath):
+                        if os.path.exists(localpath + '.' + md5):
+                            break
+                        else:
+                            os.system('rm -f %s*' % localpath)
 
-            g = pygraphviz.AGraph(string=code)
-            g.layout()
-            g.draw(localpath, prog=prog)
-            sys.stderr.write('Local Path [' + localpath + ']\n')
-            sys.stderr.write('Remote Path [' + remotepath + ']\n')
+                    dir = os.path.dirname(localpath)
+                    if not os.path.isdir(dir):
+                        os.makedirs(dir)
+
+                    os.system('touch %s.%s' % (localpath, md5))
+
+                    g = pygraphviz.AGraph(string=code)
+                    g.layout()
+                    g.draw(localpath, prog=prog)
+                    sys.stderr.write('Local Path [' + localpath + ']\n')
+                    sys.stderr.write('Remote Path [' + remotepath + ']\n')
+                except Exception as e:
+                    sys.stderr.write('{}: not found datapath in meta, please patch/run.sh\n'.format(e))
+                    exit(-1)
+                finally:
+                    break
 
             image = Image([ident, classes, keyvals], 
                           caption, 

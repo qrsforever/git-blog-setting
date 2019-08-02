8205e36c4ba709e70c3f83fe46eb4834870a6878
Li Dong <lidong8@le.com>
2019-08-02 14:04:19 +0800

    add latex graph feature in my blog

--- graphviz.py
+++ graphviz.py
@@ -15,10 +15,23 @@ import pygraphviz
 from pandocfilters import toJSONFilter, Para, Image, get_filename4code
 from pandocfilters import get_caption, get_extension, get_value
 
+tmp_path = '/tmp/gitblog'
 top_path = os.path.abspath(os.path.dirname(__file__))
 dir_name = 'graph-image'
 git_path = 'https://raw.githubusercontent.com/qrsforever/assets_blog_post/master/'
 git_post = '?sanitize=true'
+
+
+           
+doc_tmpl = r"""\documentclass[12pt,border=5pt,varwidth=true]{standalone}
+\usepackage{graphicx}
+\usepackage[utf8]{inputenc}
+\usepackage[T1]{fontenc}
+\usepackage{psfrag}
+\begin{document}
+    \input{%s}
+    \includegraphics{%s} 
+\end{document} """
 
 def graphviz(key, value, format, meta):
     if key == 'CodeBlock':
@@ -29,11 +42,16 @@ def graphviz(key, value, format, meta):
             filetype = get_extension(format, "svg", html="svg", latex="pdf")
             # dest = get_filename4code(graph, code, filetype)
             md5 = hashlib.sha1(code.encode(sys.getfilesystemencoding())).hexdigest()
-            filename, _ = get_value(keyvals, u"fileName")
-            if filename is None:
+            basename, _ = get_value(keyvals, u"fileName")
+            if basename is None:
                 sys.stderr.write('not set filename in {}\n')
                 exit(-1)
-            filename += '.' + filetype
+
+            latex, _ = get_value(keyvals, u"latex")
+            if latex is not None:
+                filetype = 'png'
+
+            filename = basename + '.' + filetype
             while True:
                 try:
                     datapath = meta['datapath']['c']
@@ -59,13 +77,34 @@ def graphviz(key, value, format, meta):
                     if not os.path.isdir(dir):
                         os.makedirs(dir)
 
-                    os.system('touch %s.%s' % (localpath, md5))
+                    if latex is None:
+                        g = pygraphviz.AGraph(string=code)
+                        g.layout()
+                        g.draw(localpath, prog=prog)
+                    else:
+                        ladot_file = os.path.join(tmp_path, '{}.ladot'.format(basename))
+                        latex_file = os.path.join(tmp_path, '{}.latex'.format(basename))
 
-                    g = pygraphviz.AGraph(string=code)
-                    g.layout()
-                    g.draw(localpath, prog=prog)
+                        with open(ladot_file, 'w') as f:
+                            f.write(code)
+
+                        with open(latex_file, 'w') as f:
+                            f.write(doc_tmpl % ('{}.tex'.format(basename), '{}.ps'.format(basename)))
+
+                        resolution, _ = get_value(keyvals, u"resolution")
+                        if resolution is None:
+                            resolution = "1200"
+                        density, _ = get_value(keyvals, u"density")
+                        if density is None:
+                            density = "200"
+
+                        os.system('{}/ladot {} {} {} {}'.format(top_path, ladot_file, tmp_path, resolution, density))
+                        if os.path.exists('%s.png' % os.path.join(tmp_path, basename)):
+                            os.system('cp %s.png %s' % (os.path.join(tmp_path, basename), localpath))
+
                     sys.stderr.write('Local Path [' + localpath + ']\n')
                     sys.stderr.write('Remote Path [' + remotepath + ']\n')
+                    os.system('touch %s.%s' % (localpath, md5))
                 except Exception as e:
                     sys.stderr.write('{}: not found datapath in meta, please patch/run.sh\n'.format(e))
                     exit(-1)
@@ -79,5 +118,6 @@ def graphviz(key, value, format, meta):
             return Para([image])
 
 if __name__ == "__main__":
-
+    if not os.path.exists(tmp_path):
+        os.mkdir(tmp_path)
     toJSONFilter(graphviz)
